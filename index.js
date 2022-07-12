94022150405def56e9706eeb366420aea311be65
devthalal <thalal.hassan@neoito.com>
2022-07-12 10:25:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const add-user = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello add-user`}))
+  res.end()
+  
+}
+
+export default add-user
